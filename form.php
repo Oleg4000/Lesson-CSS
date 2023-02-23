785268df285b5fac8a1c6c4d05f4391afc0e245f
Oleg4000 <olegklim568@gmail.com>
2023-02-23 22:10:20 +0200

    CSS test bootstrap

---
+++ form.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css2/style_form.css" />
+
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="form_add">
+    <form action="todo_car_db.php" method="POST">
+      <div class="input-field">
+        <label for="priority">Пріоритет</label>
+        <input type="text" id="priority" name="priority" />
+      </div>
+      <div class="input-field">
+        <label for="priority">Пріоритет</label>
+        <input type="text" id="priority" name="priority" />
+      </div>
+      <div class="input-field">
+        <label for="priority">Пріоритет</label>
+        <input type="text" id="priority" name="priority" />
+      </div>
+      <div class="input-field">
+        <label for="priority">Пріоритет</label>
+        <input type="text" id="priority" name="priority" />
+      </div>
+      <div class="input-field">
+        <label for="priority">Пріоритет</label>
+        <input type="text" id="priority" name="priority" />
+      </div>
+      <div class="input-field">
+        <label for="priority">Пріоритет</label>
+        <input type="text" id="priority" name="priority" />
+      </div>
+      <div class="input-field">
+        <label for="priority">Пріоритет</label>
+        <input type="text" id="priority" name="priority" />
+      </div>
+      <div class="input-field">
+        <label for="priority">Пріоритет</label>
+        <input type="text" id="priority" name="priority" />
+      </div>
+      <div class="input-field">
+        <label for="priority">Пріоритет</label>
+        <input type="text" id="priority" name="priority" />
+      </div>
+      <div class="input-field">
+        <label for="priority">Пріоритет</label>
+        <input type="text" id="priority" name="priority" />
+      </div>
+      <div class="input-field">
+        <label for="priority">Пріоритет</label>
+        <input type="text" id="priority" name="priority" />
+      </div>
+      <div class="input-field">
+        <label for="priority">Пріоритет</label>
+        <input type="text" id="priority" name="priority" />
+      </div>
+      <div class="input-field">
+        <label for="priority">Пріоритет</label>
+        <input type="text" id="priority" name="priority" />
+      </div>
+    </form>
+  </div>
+
+  <!--  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Колонка 1</th>
+          <th>Колонка 2</th>
+          <th>Колонка 3</th>
+          <th>Колонка 4</th>
+          <th>Колонка 5</th>
+          <th>Колонка 6</th>
+          <th>Колонка 7</th>
+          <th>Колонка 8</th>
+          <th>Колонка 9</th>
+          <th>Колонка 10</th>
+          <th>Колонка 11</th>
+          <th>Колонка 12</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Рядок 1, Колонка 1</td>
+          <td>Рядок 1, Колонка 2</td>
+          <td>Рядок 1, Колонка 3</td>
+          <td>Рядок 1, Колонка 4</td>
+          <td>Рядок 1, Колонка 5</td>
+          <td>Рядок 1, Колонка 6</td>
+          <td>Рядок 1, Колонка 7</td>
+          <td>Рядок 1, Колонка 8</td>
+          <td>Рядок 1, Колонка 9</td>
+          <td>Рядок 1, Колонка 10</td>
+          <td>Рядок 1, Колонка 11</td>
+          <td>Рядок 1, Колонка 12</td>
+        </tr>
+        <tr>
+          <td>Рядок 2, Колонка 1</td>
+          <td>Рядок 2, Колонка 2</td>
+          <td>Рядок 2, Колонка 3</td>
+          <td>Рядок 2, Колонка 4</td>
+          <td>Рядок 2, Колонка 5</td>
+          <td>Рядок 2, Колонка 6</td>
+          <td>Рядок 2, Колонка 7</td>
+          <td>Рядок 2, Колонка 8</td>
+          <td>Рядок 2, Колонка 9</td>
+          <td>Рядок 2, Колонка 10</td>
+          <td>Рядок 2, Колонка 11</td>
+          <td>Рядок 2, Колонка 12</td>
+        </tr>
+        
+      </tbody>
+    </table>
+
+  </div> -->
+  <div class="container">
+    <h2 class="text-center">Приклад таблиці</h2>
+    <div class="table-responsive">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Ім'я</th>
+            <th>Прізвище</th>
+            <th>Посада</th>
+            <th>Оклад</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Джон</td>
+            <td>Сміт</td>
+            <td>Менеджер</td>
+            <td>$50,000</td>
+          </tr>
+          <tr>
+            <td>Анна</td>
+            <td>Хамілтон</td>
+            <td>Розробник</td>
+            <td>$60,000</td>
+          </tr>
+          <tr>
+            <td>Пітер</td>
+            <td>Джексон</td>
+            <td>Тестувальник</td>
+            <td>$45,000</td>
+          </tr>
+          <tr>
+            <td>Мері</td>
+            <td>Сміт</td>
+            <td>Дизайнер</td>
+            <td>$55,000</td>
+          </tr>
+          <tr>
+            <td>Джек</td>
+            <td>Сімпсон</td>
+            <td>Менеджер</td>
+            <td>$70,000</td>
+          </tr>
+          <tr>
+            <td>Сара</td>
+            <td>Міллер</td>
+            <td>Розробник</td>
+            <td>$75,000</td>
+          </tr>
+          <tr>
+            <td>Том</td>
+            <td>Джонсон</td>
+            <td>Тестувальник</td>
+            <td>$50,000</td>
+          </tr>
+          <tr>
+            <td>Емма</td>
+            <td>Браун</td>
+            <td>Дизайнер</td>
+            <td>$65,000</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <div class="container2">
+    <h2 class="text-center">Приклад таблиці</h2>
+    <div class="table-responsive2">
+      <table class="table table-bordered border-primary">
+        <thead>
+          <tr>
+            <th>Ім'я</th>
+            <th>Прізвище</th>
+            <th>Посада</th>
+            <th>Оклад</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Джон</td>
+            <td>Сміт</td>
+            <td>Менеджер</td>
+            <td>$50,000</td>
+          </tr>
+          <tr>
+            <td>Анна</td>
+            <td>Хамілтон</td>
+            <td>Розробник</td>
+            <td>$60,000</td>
+          </tr>
+          <tr>
+            <td>Пітер</td>
+            <td>Джексон</td>
+            <td>Тестувальник</td>
+            <td>$45,000</td>
+          </tr>
+          <tr>
+            <td>Мері</td>
+            <td>Сміт</td>
+            <td>Дизайнер</td>
+            <td>$55,000</td>
+          </tr>
+          <tr>
+            <td>Джек</td>
+            <td>Сімпсон</td>
+            <td>Менеджер</td>
+            <td>$70,000</td>
+          </tr>
+          <tr>
+            <td>Сара</td>
+            <td>Міллер</td>
+            <td>Розробник</td>
+            <td>$75,000</td>
+          </tr>
+          <tr>
+            <td>Том</td>
+            <td>Джонсон</td>
+            <td>Тестувальник</td>
+            <td>$50,000</td>
+          </tr>
+          <tr>
+            <td>Емма</td>
+            <td>Браун</td>
+            <td>Дизайнер</td>
+            <td>$65,000</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <script src="./js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
